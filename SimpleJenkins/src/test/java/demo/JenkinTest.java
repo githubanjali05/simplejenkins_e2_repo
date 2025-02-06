f155f09def8cf7cc4a2701fb120149db67c6e24c
Anjali <Anjali@DESKTOP-DN4EF94>
2025-02-06 13:12:52 +0530

    commit jenkins

---
+++ SimpleJenkins/src/test/java/demo/JenkinTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinTest {
+	@Test
+	public void sample() {
+		Reporter.log("Lenkins class executed");
+	}
+
+}
